04bd37b28b5177751d11785ec4b5ec3a1baa8ae2
Ubuntu <ubuntu@localhost.localdomain>
2025-11-28 04:50:38 +0800

    ppidloop程序实现把父进程的pid写入ppidloop.log中

--- 2-1系统调用/ppidloop.c
+++ 2-1系统调用/ppidloop.c
@@ -3,9 +3,25 @@
 #include <stdio.h>
 
 int main(void){
-    for(;;){
-        printf("%d\n", (int)getppid());
-        fflush(stdout);
-        sleep(1);
+    // 以写入模式打开文件
+    FILE *fp = fopen("./ppidloop.log","w");
+    if(fp == NULL){
+        perror("Error opening file");
+        return 1;
     }
+    // 输出父进程的pid到文件里
+    if(fprintf(fp, "the ppid is: %d\n", (int)getppid()) < 0){
+        perror("Error: Failed to write to file");
+        // 出错时关闭文件
+        if(fclose(fp) != 0){
+            perror("Error: Failed to close file");
+        };
+        return 1;
+    };
+    // 关闭文件
+    if(fclose(fp) != 0){
+        perror("Error: Failed to close file");
+        return 1;
+    }
+    return 0;
 }
