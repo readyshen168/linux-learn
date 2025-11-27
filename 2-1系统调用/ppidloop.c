c776d1bc98856fcb0254d23b32a7c3c3009cde39
Ubuntu <ubuntu@localhost.localdomain>
2025-11-28 05:26:47 +0800

    实现无限循环测试,注意每次fprintf后都要fflush一下,这样才能避免因为无法关闭文件而造成的缓冲写入磁盘失败

--- 2-1系统调用/ppidloop.c
+++ 2-1系统调用/ppidloop.c
@@ -9,18 +9,22 @@ int main(void){
         perror("Error opening file");
         return 1;
     }
-    // 循环输出父进程的pid到文件里
-    for(int i = 0; i < 2; i++){
+    // 无限循环输出父进程的pid到文件里
+    for(;;){
         if(fprintf(fp, "the ppid is: %d\n", (int)getppid()) < 0){
             perror("Error: Failed to write to file");
             // 出错时关闭文件
-            if(fclose(fp) != 0){
-                perror("Error: Failed to close file");
-            };
-            return 1;
-        };
-    }
+            if(fclose(fp) == EOF){ // 另外一种判断错误的方式
+                perror("Error: failed to close file");
+                return 1;
+            }
+        }
+        // 每次写入后立即刷新缓冲区到磁盘
+        fflush(fp);
+        sleep(1);
+    } 
     // 关闭文件
+    // 无限循环永远都不会执行到这里了
     if(fclose(fp) != 0){
         perror("Error: Failed to close file");
         return 1;
